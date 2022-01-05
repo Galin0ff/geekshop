77aabf95b9d2c426b30990219fe3518d5ec610f1
Galin0ff <g4linoff@gmail.com>
2022-01-05 22:11:47 +0700

    Lesson 3. Some fixes

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,13 +1,10 @@
-def products(request, pk=None):
-    title = "продукты"
-    links_menu = ProductCategory.objects.all()
-    same_products = Product.objects.all()
-    content = {
-        "title": title,
-        "links_menu": links_menu,
-        "same_products": same_products,
-        "media_url": settings.MEDIA_URL,
-    }
-    if pk:
-        print(f"User select category: {pk}")
-    return render(request, "mainapp/products.html", content)
+from django.urls import path
+
+import mainapp.views as mainapp
+
+app_name = "mainapp"
+
+urlpatterns = [
+    path("", mainapp.products, name="index"),
+    path("<int:pk>/", mainapp.products, name="category"),
+]
